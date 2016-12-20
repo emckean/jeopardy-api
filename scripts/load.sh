80af464a8e39cfc22c0077157ad455ae6d1c4cee
emckean <emckean@erins-mbp-2.usca.ibm.com>
2016-12-20 11:39:27 -0800

    adds readme shell, data folder and single question file, and updates load script

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,16 +1,16 @@
-let numberofQuestions=$(cat /Users/emckean/Code/jeopardy-example/data/JEOPARDY_QUESTIONS1.json| jq '. | length')
+let numberofQuestions=$(cat /data/JEOPARDY_QUESTIONS1.json| jq '. | length')
 echo "There are $numberofQuestions questions to load ..."
 
-COUNTER=20
+COUNTER=1
 #set limit here to a reasonable number, loading all the questions like this is not reasonable
-LIMIT=50
+LIMIT=19
 
 while [  $COUNTER -lt $LIMIT ]; do
 	#this line uses jq to grab the COUNTERth item from the large json file, and then escapes single quotes before outputting to temp file
-	echo $(cat /Users/emckean/Code/jeopardy-example/data/JEOPARDY_QUESTIONS1.json | jq '.['$COUNTER']' | sed "s/'/\\\u0027/g")	> /Users/emckean/Code/jeopardy-example/data/tempfile.txt
+	echo $(cat data/JEOPARDY_QUESTIONS1.json | jq '.['$COUNTER']' | sed "s/'/\\\u0027/g")	> data/tempfile.txt
 
 	#this line posts tempfile to elasticsearch -- make sure you have the right index and collection name! it will autogenerate an id
-	curl -XPOST http://localhost:9200/jeopardy-dev/Question/ -d @"/Users/emckean/Code/jeopardy-example/data/tempfile.txt"
+	curl -XPOST http://localhost:9200/jeopardy-dev/Question/ -d @"data/tempfile.txt"
 	
 	#put in a newline for readability
 	echo ""
